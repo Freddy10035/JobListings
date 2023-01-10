701559b4cf9bad2b11a3126b72d536cd8a98c792
Fredrick Ochieng <flaughters@gmail.com>
2023-01-10 19:40:10 +0300

    Listings

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Listing extends Model
 {
     use HasFactory;
+
+
+    // public static function all() {
+    // return     
+    // }
+
+    // public static function find($id) {
+    //     $listings = self::all();
+
+    //     foreach ($listings as $listing) {
+    //         if ($listing[$id] == $id) {
+    //             return $listing;
+    //         }
+    //     }
+    // }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings',[
+        'heading' => 'Latest Listings',
+        'listings' => Listing::all()
+     ]);
+});
+
+
+Route::get('/listings/{id}', function($id){
+    return view ('listing', [
+        'listing' => Listing::find($id)
+    ]);
 });
