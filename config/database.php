a124c7fde4861db2c971d8d3e93f0a100bf028d2
Fredrick Ochieng <fredrick_ochieng@outlook.com>
2023-05-01 22:03:45 +0300

    database.php edit

--- config/database.php
+++ config/database.php
@@ -15,7 +15,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    //'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'azure'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,6 +53,25 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
+        // create for azure database
+        'azure' => [
+            'driver' => 'azure',
+            'host' => env('AZURE_MYSQL_HOST', '127.0.0.1'),
+            'port' => env('AZURE_MYSQL_PORT', '3306'),
+            'database' => env('AZURE_MYSQL_DBNAME', 'forge'),
+            'username' => env('AZURE_MYSQL_USERNAME', 'forge'),
+            'password' => env('AZURE_MYSQL_PASSWORD', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
